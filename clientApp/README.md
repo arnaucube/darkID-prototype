9770437d3f6d534a97c93043712ea60f638a962b
arnaucode <arnaucode@gmail.com>
2018-01-10 01:07:29 +0100

    started ethereum smart contract for darkID

--- clientApp/README.md
+++ clientApp/README.md
@@ -1,5 +1,1 @@
-# serverIDsign
-
-- The server where the user creates a non anonymous account
-- Also is the server that blind signs the Anonymous ID of the users
-- Have the webapp (frontend) to interact through a GUI interface
+# clientApp
---
+++ eth/darkID_contract.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4;
+
+
+contract DarkID {
+
+    struct IdStruct {
+        string pubK;
+        string date;
+        string hashed;
+        string unblindedSig;
+        string serverVerifier;
+        string signerID;
+    }
+
+
+    IdStruct ID;
+
+
+    function DarkID(string _pubK, string _hashed, string _unblindedSig, string _serverVerifier, string _signerID) public {
+        ID = IdStruct(_pubK, "this will be the date", _hashed, _unblindedSig, _serverVerifier, _signerID);
+    }
+
+
+    function getDarkID() public constant returns(string, string, string, string, string, string) {
+        return (ID.pubK, ID.date, ID.hashed, ID.unblindedSig, ID.serverVerifier, ID.signerID);
+    }
+}
